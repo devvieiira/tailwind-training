7c49f729a94322da150f08f8b0110fd19f9b03d8
devvieiira <kauavieira900@gmail.com>
2024-05-24 19:58:37 -0300

    feat: create slot API

--- src/components/Form/Button.tsx
+++ src/components/Form/Button.tsx
@@ -12,7 +12,7 @@ const button = tv({
 		variant: {
 			primary: "bg-violet-600 text-white hover:bg-violet-700",
 			outline: "border border-zinc-300 text-zinc-900 hover:bg-zinc-100",
-			ghost: "rounded-md px-2 hover:bg-zinc-50 shadow-none",
+			ghost: "rounded-md px-2 hover:bg-zinc-50 shadow-none text-zinc-500",
 		},
 	},
 	defaultVariants: {
@@ -22,8 +22,8 @@ const button = tv({
 
 type ButtonProps = ComponentProps<"button"> & VariantProps<typeof button>;
 
-const Button = ({ variant, ...props }: ButtonProps) => {
-	return <button {...props} className={button({ variant })} />;
+const Button = ({ variant, className, ...props }: ButtonProps) => {
+	return <button {...props} className={button({ variant, className })} />;
 };
 
 export default Button;
