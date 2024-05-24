42a694d71ffe003f08d925142bb7abfd89d9bed6
devvieiira <kauavieira900@gmail.com>
2024-05-24 00:53:03 -0300

    feat: create radix tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import Image from "next/image";
+import SettingsTabs from "@/components/SettingsTabs";
 
 export default function Home() {
-	return <h1>Hello Tailwind</h1>;
+	return (
+		<>
+			<h1 className="text-3xl font-bold text-zinc-900">Settings</h1>
+
+			<SettingsTabs />
+		</>
+	);
 }
