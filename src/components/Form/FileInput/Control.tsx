407de60bab51fb73ad5141e552a694e60be56bff
devvieiira <kauavieira900@gmail.com>
2024-05-24 14:48:57 -0300

    feat: componentizing file input

---
+++ src/components/Form/FileInput/Control.tsx
@@ -0,0 +1,6 @@
+import type { ComponentProps } from "react";
+
+type ControlProps = ComponentProps<"input">;
+export const Control = (props: ControlProps) => {
+	return <input type="file" className="sr-only" id="photo" {...props} />;
+};
---
+++ src/components/Form/FileInput/index.tsx
@@ -0,0 +1,6 @@
+import { Control } from "./Control";
+import { Root } from "./Root";
+import { Trigger } from "./Trigger";
+import { ImagePreview } from "./imagePreview";
+
+export { Control, ImagePreview, Root, Trigger };
